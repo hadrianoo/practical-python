18c6c208333b791d842790e31d4ad66aad4c7cbb
hadrianoo <57418210+hadrianoo@users.noreply.github.com>
2022-10-17 14:57:41 +0200

    1.7 functions

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,11 +2,26 @@
 #
 # Exercise 1.27
 
-with open("Data/portfolio.csv", "rt") as f:
-    headers = next(f)
-    cost = 0
-    for line in f:
-        row = line.split(",")
-        print(row)
-        cost += int(row[1]) * float(row[2])
-    print('Total cost', cost)
+import csv
+import sys
+
+
+def portfolio_cost(filename):
+    cost_total = 0
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for line in rows:
+            try:
+                cost_total += int(line[1]) * float(line[2])
+            except ValueError:
+                print("Can not read this", line)
+    return cost_total
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input("file path: ")
+cost = portfolio_cost(filename)
+print('Total cost', cost)
