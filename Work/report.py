379f6f9d8fd034b68c1d23974fbff79f63048893
hadrianoo <57418210+hadrianoo@users.noreply.github.com>
2022-10-17 19:14:20 +0200

    2.1 datatypes

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,48 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+
+def read_portfolio(filename):
+    with open(filename, "rt") as f:
+        portfolio = []
+        rows = csv.reader(f)
+        headers = next(rows)
+        for name, shares, price in rows:
+            row = {"name": name, "shares": int(shares), "price": float(price)}
+            portfolio.append(row)
+    return portfolio
+
+
+def read_prices(filename):
+    with open(filename, "rt") as f:
+        prices = {}
+        rows = csv.reader(f)
+        for data in rows:
+            try:
+                prices[data[0]] = float(data[1])
+            except IndexError:
+                pass
+    return prices
+
+
+def current_price(filename1, filename2):
+    prices = read_prices(filename1)
+    portfolio = read_portfolio(filename2)
+
+    total_cost = 0.0
+    for s in portfolio:
+        total_cost += s['shares'] * s['price']
+    print('Total cost', total_cost)
+
+    total_value = 0.0
+    for s in portfolio:
+
+        total_value += s['shares'] * prices[s['name']]
+
+    print('Current value', total_value)
+    print('Gain', total_value - total_cost)
+
+current_price("data/prices.csv", "data/portfolio.csv")
