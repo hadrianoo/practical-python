78b71e8ce395702ab1c0d585f253ffa1dece5b4d
hadrianoo <57418210+hadrianoo@users.noreply.github.com>
2022-10-17 11:53:01 +0200

    lists

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open("Data/portfolio.csv", "rt") as f:
+    headers = next(f)
+    cost = 0
+    for line in f:
+        row = line.split(",")
+        print(row)
+        cost += int(row[1]) * float(row[2])
+    print('Total cost', cost)
