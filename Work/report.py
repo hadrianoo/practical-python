e343c7a43e79fad4678323e13721320f959330d0
Adrian <hadriano.hadriano@gmail.com>
2022-10-20 11:23:19 +0200

    2.3 formating

--- Work/report.py
+++ Work/report.py
@@ -3,6 +3,7 @@
 # Exercise 2.4
 
 import csv
+import locale
 
 
 def read_portfolio(filename):
@@ -28,21 +29,42 @@ def read_prices(filename):
     return prices
 
 
-def current_price(filename1, filename2):
-    prices = read_prices(filename1)
-    portfolio = read_portfolio(filename2)
+# def current_price(filename1, filename2):
+#     prices = read_prices(filename1)
+#     portfolio = read_portfolio(filename2)
+#
+#     total_cost = 0.0
+#     for s in portfolio:
+#         total_cost += s['shares'] * s['price']
+#     print('Total cost', total_cost)
+#
+#     total_value = 0.0
+#     for s in portfolio:
+#
+#         total_value += s['shares'] * prices[s['name']]
+#
+#     print('Current value', total_value)
+#     print('Gain', total_value - total_cost)
+#
+# current_price("data/prices.csv", "data/portfolio.csv")
 
-    total_cost = 0.0
-    for s in portfolio:
-        total_cost += s['shares'] * s['price']
-    print('Total cost', total_cost)
 
-    total_value = 0.0
-    for s in portfolio:
+def make_report(portfolio, prices):
+    report = []
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    for line in portfolio:
+        line = (line["name"], line["shares"], prices[line["name"]], prices[line["name"]] - line["price"])
+        report.append(line)
+    return report
 
-        total_value += s['shares'] * prices[s['name']]
 
-    print('Current value', total_value)
-    print('Gain', total_value - total_cost)
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+report = make_report(portfolio, prices)
 
-current_price("data/prices.csv", "data/portfolio.csv")
+headers = ('Name', 'Shares', 'Price', 'Change')
+print(f"{headers[0]:>10s}{headers[1]:>10s}{headers[2]:>10s}{headers[3]:>10s}")
+print('---------- --------- --------- ----------')
+for name, shares, price, change in report:
+    print(f"{name:>10s}{shares:>10d}{price:>10.2f}{change:>10.2f}")
+
